7337a8eb71c0faea7bdd7ae609af3aa85124a680
Rhys <rhys.al.evans@proton.me>
2026-05-13 10:46:48 +0200

    Initial local commit

---
+++ data/test_file1.py
@@ -0,0 +1,10 @@
+this is a nothing Python file
+
+
+# TODO! py should be line 1
+do a thing
+
+do another thing
+
+# TODO! py should be line 2
+final line
---
+++ data/test_file2.rs
@@ -0,0 +1,10 @@
+this is a nothing Rust file
+
+
+// TODO! rs should be line 1
+do a thing
+
+do another thing
+
+//TODO! rs should be line 2
+final line
