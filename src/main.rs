1d2967b2bce5916528fb38b51c8b2b2d4bd3c248
Rhys <rhys.al.evans@proton.me>
2026-05-13 16:48:47 +0200

    Added clap for argparse, options for dir, ext ans search

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use anyhow::{Context, Result};
+use clap::Parser;
 use std::io::{BufRead, BufReader};
 use std::{fs::File, path::Path};
 use walkdir::{DirEntry, WalkDir};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Directory to recursively search in
+    #[arg(short, long, default_value = ".")]
+    dir: String,
+    /// File extension to filter check in (e.g., "rs", "py", "txt")
+    #[arg(short, long)]
+    ext: String,
+    /// Macro-style tag to search for
+    #[arg(short, long, default_value = "TODO!")]
+    search: String,
+}
 
 fn dir_filter(entry: &DirEntry, ext: &str) -> bool {
     let path = entry.path();
@@ -13,12 +28,29 @@ fn dir_filter(entry: &DirEntry, ext: &str) -> bool {
     }
 }
 
-fn main() -> Result<()> {
-    println!("Hello, world!");
+fn get_comment_str(ext: &str) -> &'static str {
+    match ext {
+        // C-style comments with //
+        "rs" | "c" | "cpp" | "h" | "hpp" | "js" | "ts" | "java" | "go" | "cs" => "//",
 
-    let dir = Path::new("/home/revans/personal/todo/data");
-    let ext = "py";
-    let search_str = "# TODO!";
+        // Python-style comments with #
+        "py" | "sh" | "rb" | "yaml" | "yml" | "toml" | "pl" => "#",
+
+        // Lua-style comments with --
+        "sql" | "lua" | "hs" => "--",
+
+        // Default = without comment (e.g. markdown/txt)
+        _ => "",
+    }
+}
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    println!("todo - Starting compilation...");
+
+    let ext = args.ext.trim_start_matches('.');
+    let dir = Path::new(&args.dir);
+    let search_str = format!("{} {}", get_comment_str(ext), args.search);
 
     for entry in WalkDir::new(dir)
         .into_iter()
@@ -31,19 +63,19 @@ fn main() -> Result<()> {
             && let Some(file_ext) = path.extension()
             && file_ext.to_string_lossy() == ext
         {
-            println!("Found file: {:?}", &path);
-            let file =
-                File::open(path).with_context(|| format!("Failed to open file: '{:?}'", &path))?;
+            println!("Found file: {}", &path.display());
+            let file = File::open(path)
+                .with_context(|| format!("Failed to open file: '{:?}'", &path.display()))?;
             let reader = BufReader::new(file);
             for (i, line) in reader.lines().enumerate() {
                 match line {
                     Ok(str) => {
-                        if str.contains(search_str) {
+                        if str.contains(&search_str) {
                             println!(
                                 "{} : Line {} - {}",
                                 &path.display(),
                                 i,
-                                &str.trim().trim_start_matches(search_str)
+                                &str.trim().trim_start_matches(&search_str)
                             );
                         }
                     }
